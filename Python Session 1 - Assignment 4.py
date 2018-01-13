70763455796187e583535a9bc15e6b0303c4803d
sripadh4 <35394080+sripadh4@users.noreply.github.com>
2018-01-13 15:40:05 +0530

    Add files via upload

---
+++ Python Session 1 - Assignment 4.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[15]:
+
+import math
+Diameter = 12
+Radius = Diameter/2
+a = 4/3
+b = math.pi
+c = Radius**3
+Volume_of_Sphere = a*b*c
+print("Volume of Sphere = ", Volume_of_Sphere)
+
