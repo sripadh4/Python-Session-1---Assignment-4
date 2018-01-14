98668d49e740af50e827f83ff55eccd4c959980b
sripadh4 <35394080+sripadh4@users.noreply.github.com>
2018-01-14 12:21:34 +0530

    Add files via upload

--- Python Session 1 - Assignment 4.py
+++ Python Session 1 - Assignment 4.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[15]:
+# In[2]:
 
-import math
+import numpy as np
 Diameter = 12
 Radius = Diameter/2
 a = 4/3
-b = math.pi
+b = np.pi
 c = Radius**3
 Volume_of_Sphere = a*b*c
 print("Volume of Sphere = ", Volume_of_Sphere)
